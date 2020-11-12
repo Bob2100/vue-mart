c75d67dd554909a2d1985a1398e60bf283bfbedc
Bob <bob_w@qq.com>
2020-11-12 23:19:21 +0800

    登录校验

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,15 +1,94 @@
 <template>
   <div>
-    Login
+    <cube-form
+      :model="model"
+      :schema="schema"
+      @validate="validateHandler"
+      @submit="submitHandler"
+    ></cube-form>
   </div>
 </template>
 
 <script>
-  export default {
-    
-  }
+export default {
+  data() {
+    return {
+      model: {
+        username: "",
+        password: "",
+      },
+      schema: {
+        fields: [
+          {
+            type: "input",
+            modelKey: "username",
+            label: "用户名",
+            props: {
+              placeholder: "请输入用户名",
+            },
+            rules: {
+              required: true,
+            },
+            trigger: "blur",
+            messages: {
+              required: "用户名为必填项",
+            },
+          },
+          {
+            type: "input",
+            modelKey: "password",
+            label: "密码",
+            props: {
+              placeholder: "请输入密码",
+              type: "password",
+              eye: {
+                open: false,
+              },
+            },
+            rules: {
+              required: true,
+            },
+            trigger: "blur",
+            messages: {
+              required: "密码为必填项",
+            },
+          },
+          {
+            type: "submit",
+            label: "登录",
+          },
+        ],
+      },
+    };
+  },
+  methods: {
+    async submitHandler(e) {
+      e.preventDefault();
+
+      const res = await this.$http.get("/api/login", {
+        params: {
+          ...this.model,
+        },
+      });
+      const { code, token, message } = res.data;
+      if (code == 0) {
+        localStorage.setItem("token", token);
+        this.$store.commit("setToken", token);
+        this.$router.push(this.$route.query.redirect || "/");
+      } else {
+        const toast = this.$createToast({
+          time: 2000,
+          txt: message || "登录失败",
+          type: "error",
+        });
+        toast.show();
+      }
+    },
+    validateHandler(ret) {
+      console.log("校验" + JSON.stringify(ret));
+    },
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
--- vue.config.js
+++ vue.config.js
@@ -14,5 +14,42 @@ module.exports = {
       postCompile: true,
       theme: true
     }
+  },
+  configureWebpack: {
+    devServer: {
+      before(app) {
+        app.get("/api/goods", function (req, res) {
+          function* IdGenerator() {
+            let id = 0;
+            while (true) {
+              yield id++;
+            }
+          }
+          const idGenerator = IdGenerator();
+          res.json({
+            code: 0,
+            list: [
+              { id: idGenerator.next().value, text: "Java架构师", price: 200 },
+              { id: idGenerator.next().value, text: "前端架构师", price: 20000 },
+              { id: idGenerator.next().value, text: "PHP架构师", price: 100 }
+            ]
+          });
+        });
+        app.get("/api/login", function (req, res) {
+          const { username, password } = req.query;
+          if (username == 'bob' && password == '123') {
+            res.json({
+              code: 0,
+              token: 'jilei'
+            });
+          } else {
+            res.json({
+              code: 1,
+              message: '用户名或密码错误'
+            });
+          }
+        });
+      }
+    }
   }
 }
