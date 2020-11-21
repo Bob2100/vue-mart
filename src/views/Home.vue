474004cd92432a1b0253f21d1d3c71ea777381fb
Bob <bob_w@qq.com>
2020-11-21 10:26:28 +0800

    轮播图

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="home">
-    Home
+    <cube-slide ref="slide" :data="items" class="mart_slide">
+      <cube-slide-item v-for="(item, index) in items" :key="index">
+        <router-link :to="`/detail/${item.id}`">
+          <img :src="item.img" class="mart_slide-img" />
+        </router-link>
+      </cube-slide-item>
+    </cube-slide>
   </div>
 </template>
 
@@ -8,9 +14,25 @@
 // @ is an alias to /src
 export default {
   name: "Home",
+  data() {
+    return {
+      items: [],
+    };
+  },
   async created() {
-    const ret = await this.$http.get("/api/goods");
-    console.log(ret);
+    const {
+      data: { slider },
+    } = await this.$http.get("/api/goods");
+    this.items = slider;
   },
 };
 </script>
+<style lang="less" scoped>
+.mart_slide {
+  height: auto;
+}
+.mart_slide-img {
+  width: 100%;
+  height: 200px;
+}
+</style>
