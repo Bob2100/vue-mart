8a90502fe00bf9dbd86dfa3873f4775276c92140
Bob <bob_w@qq.com>
2020-11-21 17:17:43 +0800

    商品列表

---
+++ src/components/GoodList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div v-for="good in goods" :key="good.id">
+      {{ good.title }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["goods"],
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,23 +7,43 @@
         </router-link>
       </cube-slide-item>
     </cube-slide>
+    <good-list :goods="filterGoods"></good-list>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import GoodList from "@/components/GoodList.vue";
 export default {
   name: "Home",
+  components: {
+    GoodList,
+  },
   data() {
     return {
       items: [],
+      goods: {},
+      keys: [],
+      selectedKeys: [],
     };
+  },
+  computed: {
+    filterGoods() {
+      let  ret = [];
+      this.selectedKeys.forEach((key) => {
+        ret = ret.concat(this.goods[key]);
+      });
+      return ret;
+    },
   },
   async created() {
     const {
-      data: { slider },
+      data: { slider, keys, data: goods },
     } = await this.$http.get("/api/goods");
     this.items = slider;
+    this.keys = keys;
+    this.goods = goods;
+    this.selectedKeys = [...keys];
   },
 };
 </script>
